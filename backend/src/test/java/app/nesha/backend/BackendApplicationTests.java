813c249ce855ab017bd91e148d7bb5742f681cb1
Erickson Azevedo <erickson.tulio@academico.ifpb.edu.br>
2024-01-02 13:58:45 -0300

    inital setup

---
+++ backend/src/test/java/app/nesha/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.nesha.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
